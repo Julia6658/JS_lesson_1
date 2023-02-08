4e3e514691a34cb5fb8a00180d65955c9bf05832
Пользователь <jp@artismedia.by>
2023-02-08 22:34:43 +0300

    JS 1 коммит

---
+++ script.js
@@ -0,0 +1,57 @@
+//1. задание
+let cleanSalary = 40000,
+    teacherSalary = 22000;
+
+let newSalary = cleanSalary + teacherSalary;
+
+console.log(newSalary);
+
+//2. Задание
+let song = "Вдадивосток " + 2000;
+console.log(song);
+
+// 3 Задание
+let messenger = 128,
+    like = 254,
+    vk = 137,
+    youtube = 201;
+
+let timeWaste = messenger + like + vk + youtube;
+
+console.log("Прокрастинировала " + timeWaste + " минут");
+
+// 4 Задание
+console.log(2 ** 5);
+
+// 5 Задание
+console.log("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!")
+
+// 6 Задание
+const greetings = "Привет";
+const name = "Кот";
+alert(`${greetings}, ${name}`);
+
+// 7 Задание
+let time;
+time = 34;
+console.log(time);
+
+alert(`Время в пути ${time} минуты`);
+console.log('Старт поездки. Осталось минут: ' + time);
+time = time - 15;
+console.log('Немного сторис в соцсетях. Осталось минут: ' + time);
+time = time - 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+
+// 8 Задание
+
+const fahrenheit = 451;
+const celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту — это ${celsius} градуса по Цельсию`);
+
+
+
+
+
